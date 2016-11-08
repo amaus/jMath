d57d874325582f197ce7b2769330aefb52b36d71
Aaron Maus <amaus@uno.edu>
2016-11-08 16:21:24 -0600

    added two executables to jMath. Both to read in dimacs files and find cliques. also added into build.gradle the ability to provide those executables in a distribution.

---
+++ src/main/java/com/aaronpmaus/jMath/executables/FindClique.java
@@ -0,0 +1,49 @@
+package com.aaronpmaus.jMath.executables;
+import com.aaronpmaus.jMath.graph.*;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Date;
+
+public class FindClique {
+    public static void main(String[] args){
+        if(args.length != 2){
+            System.out.println("Usage: FindClique dimacsfilename cliqueSize");
+            System.exit(1);
+        }
+        String filename = args[0];
+        int k = Integer.parseInt(args[1]);
+
+        // Read in the graph from the DIMACS file and print the
+        // max possible clique number
+        UndirectedGraph<Integer> graph = GraphIO.readFromDimacsFile(filename);
+        System.out.println("Graph built from dimacs file: " + filename);
+        int maxPossibleCliqueNum = graph.maxPossibleCliqueNumDeep(graph);
+        System.out.println("Max Possible Clique Number: " + maxPossibleCliqueNum);
+
+        //System.out.println(graph);
+        //neighborhood = graph.getNeighborhood(graph.getNode(new Integer(3)));
+        //System.out.println("Neighborhood around 3:\n"+neighborhood);
+        //System.out.println("Is Clique?: " + graph.isClique(neighborhood));
+        
+        // run the find clique algorithm while clocking it to know how much time it took
+        long startTime = new Date().getTime();
+        UndirectedGraph<Integer> maxClique = graph.findClique(graph, k, 1);
+        long endTime = new Date().getTime();
+        System.out.println("Runtime: " + (endTime-startTime) + " milliseconds");
+        
+        // Print out the max clique
+        if(maxClique != null){
+            // build a clique string containing the IDs of all the nodes in this clique
+            String cliqueStr = "";
+            for(Node<Integer> node : maxClique.getNodes()){
+                cliqueStr += node.get() + " ";
+            }
+            System.out.println(cliqueStr);
+            System.out.println(maxClique.size() + " nodes in clique");
+            System.out.print(maxClique);
+        } else {
+            System.out.println("There are no cliques of size " + k);
+        }
+        System.out.println(UndirectedGraph.numRecursiveCalls + " RECURSIVE CALLS MADE.");
+    }
+}
---
+++ src/main/java/com/aaronpmaus/jMath/executables/FindMaxClique.java
@@ -0,0 +1,42 @@
+package com.aaronpmaus.jMath.executables;
+import com.aaronpmaus.jMath.graph.*;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Date;
+import java.util.Collections;
+
+public class FindMaxClique{
+    public static void main(String[] args){
+        if(args.length != 1){
+            System.out.println("Usage: FindMaxClique dimacsfilename");
+            System.exit(1);
+        }
+        String filename = args[0];
+        UndirectedGraph<Integer> graph = GraphIO.readFromDimacsFile(filename);
+        System.out.println("Graph built from dimacs file: " + filename);
+        System.out.println("Graph Density: " + graph.density());
+        //System.out.println(graph);
+        //neighborhood = graph.getNeighborhood(graph.getNode(new Integer(3)));
+        //System.out.println("Neighborhood around 3:\n"+neighborhood);
+        //System.out.println("Is Clique?: " + graph.isClique(neighborhood));
+        UndirectedGraph<Integer> maxClique = graph.findMaxClique(graph);
+        if(maxClique != null){
+            String cliqueStr = "";
+            ArrayList<Integer> nodeNums = new ArrayList<Integer>();
+            for(Node<Integer> node : maxClique.getNodes()){
+                nodeNums.add(node.get());
+            }
+            Collections.sort(nodeNums);
+            for(Integer i : nodeNums){
+                cliqueStr += i + " ";
+            }
+            System.out.println("MAXIMUM CLIQUE");
+            System.out.print(maxClique);
+            System.out.println("CLIQUE: "+cliqueStr);
+            System.out.println(maxClique.size() + " nodes in clique");
+        } else {
+            System.out.println("Max clique not found. Umm.. somethings wrong");
+        }
+        System.out.println(UndirectedGraph.numRecursiveCalls + " RECURSIVE CALLS MADE.");
+    }
+}
